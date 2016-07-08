78e20624b9a71757f4c8826c884df9102b7cf206
kcrong <kcrong@kim82536.pe.kr>
2016-07-08 21:10:06 +0900

    Add print_bpf_u_int32 func (print bpf_u_int32 var pretty) Move location of Good Info Comments

--- main.c
+++ main.c
@@ -5,26 +5,36 @@
  * 네트워크 라이브러리 pcap 을 이용해 패킷을 캡쳐한 뒤 ethernet, ip, tcp 헤더에서 source mac, ip, port 와 destination mac, ip, port 추출해서 출력.
  */
 
+/*
+ * Good Info:
+ * https://wiki.kldp.org/KoreanDoc/html/Libpcap-KLDP/function.html --> pcap 함수에 대한 한글설명
+ * http://www.tcpdump.org/pcap.html --> 영문자료. pcap 함수에 대해 자세히 나와있음
+ * http://mindnet.tistory.com/entry/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC-%EC%89%BD%EA%B2%8C-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0-18%ED%8E%B8-IP-Header-IP%ED%97%A4%EB%8D%94-%EA%B5%AC%EC%A1%B0
+   --> 패킷 헤더 자료
+ */
 
 /*
  * This Program need Root or Administrator Permission
  */
 
-
 #include <stdio.h>
 #include <pcap.h>
 #include <unistd.h>
 
+struct data_form {
+    unsigned char one;
+    unsigned char two;
+    unsigned char three;
+    unsigned char four;
+};
+
+void print_bpf_u_int32(bpf_u_int32 data){
+    struct data_form *ptr=(struct data_form*)&data;
+
+    printf("%d.%d.%d.%d\n", ptr->one, ptr->two, ptr->three, ptr->four);
+}
+
 int main(int argc, char **argv) {
-    /*
-     * Good Info:
-     * https://wiki.kldp.org/KoreanDoc/html/Libpcap-KLDP/function.html --> pcap 함수에 대한 한글설명
-     * http://www.tcpdump.org/pcap.html --> 영문자료. pcap 함수에 대해 자세히 나와있음
-     * http://mindnet.tistory.com/entry/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC-%EC%89%BD%EA%B2%8C-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0-18%ED%8E%B8-IP-Header-IP%ED%97%A4%EB%8D%94-%EA%B5%AC%EC%A1%B0
-       --> 패킷 헤더 자료
-     *
-     *
-     */
 
     // Only Root User
     if(0 != getuid()) {
@@ -51,6 +61,9 @@ int main(int argc, char **argv) {
             &subnet_mask,
             error_buf
     );
+
+    print_bpf_u_int32(network_addr);
+    print_bpf_u_int32(subnet_mask);
 
 
     pcap_t *device;  // pcap_t *pcap_open_live(const char *, int, int, int, char *);

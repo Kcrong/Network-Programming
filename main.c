559357e748cf3ed0d02c88a1b1fd9b9b4cb018d2
kcrong <kcrong@kim82536.pe.kr>
2016-07-09 23:55:23 +0900

    Add print error when lookupnet failed

--- main.c
+++ main.c
@@ -65,7 +65,7 @@ int main(int argc, char **argv) {
             error_buf
     )) {
         // if Fail
-        printf("lookupnet fail\n");
+        printf("lookupnet fail\n%s\n", error_buf);
         return 1;
     }
 
